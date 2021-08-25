660e08cfa8d4b75d93649c300c039f86fe24333e
OderoOluoch <oderoluoch@gmail.com>
2021-08-26 00:15:09 +0300

    We are able to view list of data, view data by id and create an item

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.getRoutes)
+    path('',views.getRoutes),
+    path('notes/', views.getNotes),
+    path('notes/create/', views.createNote),
+    path('notes/<str:pk>/', views.getNote),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import NoteSerializer
+from .models import Note
 
 
 @api_view(['GET'])
@@ -37,3 +39,29 @@ def getRoutes(request):
         },
     ]
     return Response(routes)
+
+
+@api_view(['GET'])
+def getNotes(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
+
+
+
+@api_view(['GET'])
+def getNote(request, pk):
+    note = Note.objects.get(id=pk)
+    serializer = NoteSerializer(note, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def createNote(request):
+    data = request.data
+
+    note = Note.objects.create(
+        body=data['body']
+    )
+    serializer = NoteSerializer(note, many=False)
+    return Response(serializer.data)
