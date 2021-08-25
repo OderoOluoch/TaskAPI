ba135aa8321ef91083f0f7ba1cc81c2d539b94bf
OderoOluoch <oderoluoch@gmail.com>
2021-08-25 22:36:20 +0300

    Created sample Json response to act as documentation for the Django API

--- api/views.py
+++ api/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def getRoutes(request):
+    routes = [
+        {
+            'EndPoint':'/notes/',
+            'method':'GET',
+            'body':None,
+            'description':'Returns an array of notes'
+        },
+         {
+            'EndPoint':'/notes/id',
+            'method':'GET',
+            'body':None,
+            'description':'Returns a single notes object based on id'
+        },
+         {
+            'EndPoint':'/notes/create',
+            'method':'POST',
+            'body':{'body':""},
+            'description':'Creates a new notes object with data send using POST request'
+        },
+         {
+            'EndPoint':'/notes/id/update',
+            'method':'PUT',
+            'body':{'body':""},
+            'description':'Updates Existing Notes Object with new data coming in from Post request'
+        },
+         {
+            'EndPoint':'/notes/id/delete',
+            'method':'DELETE',
+            'body':None,
+            'description':'Deletes an exiting notes object based on provided id'
+        },
+    ]
+    return JsonResponse(routes, safe=False)
