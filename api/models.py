12ef2872dd1bc10e29bd873090c740ad64e16880
OderoOluoch <oderoluoch@gmail.com>
2021-08-26 15:43:04 +0300

    Model refactor

--- api/models.py
+++ api/models.py
@@ -1,12 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class Note(models.Model):
-    body = models.TextField()
+class Task(models.Model):
+    #user = models.ForeignKey(User, on_delete= models.CASCADE, null = TRue, blank=True)
+    title = models.CharField(max_lenght=200)
+    detail = models.TextField(null=True, blank = True)
+    complete = models.BooleanFiled(default=False)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.body[0:50]
+        return self.title
 
     class Meta:
-        ordering = ['-updated']
+        ordering = ['completed']
